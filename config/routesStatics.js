118969f2253514f06fbf18a56bb426886138e88d
Caio Humberto Francisco <caio.in.dust@gmail.com>
2016-12-30 17:14:15 -0200

    Organização das rotas, Alteração no layout de login

---
+++ config/routesStatics.js
@@ -0,0 +1,22 @@
+﻿var path = require('path');
+var FalastraoEventsDB = require('./../models/FalastraoEvents');
+var Sugar = require('sugar');
+var q = require('q');
+Sugar.extend();
+
+module.exports = function(app, express, signalR, passport, dirname) {
+
+    app.use(express.static(dirname));
+    app.use(express.static(path.join(dirname, "download")));
+    app.use(express.static(path.join(dirname, "scripts")));
+    app.use(express.static(path.join(dirname, "content")));
+
+    app.get('/', function(req, res) {
+        res.render('index.ejs', {
+            models: {
+                title: 'Home',
+                user: req.user
+            }
+        });
+    });
+};
